ac4fc238d3c46f2aad6a32fd2049efc53e023d27
Aiman Ahmed <aimanahmed61@gmail.com>
2025-02-16 20:52:00 +0000

    CREATE: question1.py file EDIT:completed question 1 tasks

---
+++ Exercise12/question1.py
@@ -0,0 +1,25 @@
+cheese = ['Cheddar', 'Stilton', 'Cornish Yarg']
+# cheese is a list that is mutable
+# contains three substrings already
+
+# cheese += 'Oke'
+# treats the string as separate characters
+# output value is 'O','k','e'
+# need to put brackets around ['Oke']
+
+# print(cheese)
+
+cheese += ['Oke']
+# += is an augmented assignment operator - assigns value to the variable
+# [] are required so 'Oke' is treated as a single element
+# adds on to the right of the list
+
+print(cheese)
+
+# adding two values
+
+cheese.extend(['Feta', 'Brie'])
+# method (.extend) only takes one argument
+# [] creates one argument of list values
+
+print(cheese)
