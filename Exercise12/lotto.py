824eda10fa76e578ec98df675e7ed3869b67b3e9
Aiman Ahmed <aimanahmed61@gmail.com>
2025-02-17 19:22:38 +0000

    CREATE: added lotto.py and exception_handling.py file

---
+++ Exercise12/lotto.py
@@ -0,0 +1,21 @@
+import random
+# imports module of code
+help(random)
+
+lottery_numbers = random.sample(range(1,51),6)
+# variable is lottery_numbers
+# range is between integers, 1 to 51 (end value +1) otherwise end value is excluded
+# random.sample is a function for sequences -  returns a list of unique elements chosen by population sequence
+# random.sample = (population,k)
+# a part of the random module
+# returns a new list, leaving original population unchanged
+# k = length of list
+# list keeps the same order - can also be sorted and is a more flexible data structure (mutable)
+# allows multiple data items to be extracted
+
+print(lottery_numbers)
+print(type(lottery_numbers))
+# a list
+
+print("Your lottery numbers are:", lottery_numbers)
+# print a string and the output to the lottery_numbers variable
---
+++ exception_handling/exception_handling.py
@@ -0,0 +1,49 @@
+names_tuple = ('Rod', 'Jane', 'Freddy')
+# the variable is a tuple - immutable
+
+try:
+    print("####### TRY #######")
+    print("The TRY block attempts to run")
+    print(f"Original Tuple: {names_tuple}")
+    # try block contains code to be tested - if any code raises exception - it can be trapped and handled by except block
+
+    names_sorted_as_list = sorted(names_tuple)
+    print(names_sorted_as_list)
+    # sorted function returns tuple as list in ascending order
+
+    names_sorted_as_list.append("Bungle")
+    print("Added Bungle:", names_sorted_as_list)
+    # method (.append) adds string to the end of the list (mutable)
+    print("Attempt to manipulate the tuple...")
+
+    names_tuple[0] = 'Zippy'  # This will raise a TypeError
+    # attempting to change first item in list to 'Zippy'
+    # cannot add data to a tuple like you can in a list - Type Error - tuples are immutable
+
+    print("Is this code reached?")
+    # print statement is not executed because of the above error
+
+except FileNotFoundError as error:
+    print("####### EXCEPT: FileNotFoundError #######")
+    # this print statement runs as part of the except block
+    print("The EXCEPT / CATCH block only runs if this error happens")
+    print(f"The following file can not be found: {error.filename}. Please try another file")
+    # code is not handling files
+except TypeError as error:
+    print("####### EXCEPT: TypeError #######")
+    print("Oh dear, that is not allowed on that type")
+    print(error)
+except Exception as error:
+    print("####### EXCEPT: Exception #######")
+    print("Generic catch-all except / catch block")
+    print(error)
+finally:
+    # always executes even if there wasn't an error
+    # Always close file handle after use
+    print("The FINALLY block ALWAYS runs")
+    print("The finally block is used to tidy up")
+
+    if names_tuple:
+        names_tuple = None
+
+print("After exception handling is finished...the program can continue")
