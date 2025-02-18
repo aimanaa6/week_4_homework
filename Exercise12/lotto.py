2d07850896afd8861726150b764ece3e284f7d67
Aiman Ahmed <aimanahmed61@gmail.com>
2025-02-18 17:33:57 +0000

    EDIT: amended comments in lotto.py

--- Exercise12/lotto.py
+++ Exercise12/lotto.py
@@ -6,7 +6,7 @@ import random
 lottery_numbers = random.sample(range(1,51),6)
 # variable is lottery_numbers
 # range is between integers, 1 to 51 (end value +1) otherwise end value is excluded (counts up from zero)
-# random.sample is a function for sequences -  returns a list of unique elements chosen by population sequence
+# random.sample is a function for sequences -  returns a list of unique elements chosen from population sequence
 # random.sample = (population,k)
 # a part of the random module
 # k = length of list
@@ -20,4 +20,4 @@ print(type(lottery_numbers))
 # a list
 
 print("Your lottery numbers are:", lottery_numbers)
-# print a string and the output to the lottery_numbers variable
+# print a string and the output of the variable
